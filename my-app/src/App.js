bd1e3c2c3372ae4f5f3627116beb2af4aa2faa50
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2022-05-21 11:07:51 +0200

    Design updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,7 @@ function App() {
       axios.get(url).then((response) => {
         setData(response.data)
         console.log(response.data)
+        console.log(location)
       })
       setLocation('')
     }
@@ -27,41 +28,31 @@ function App() {
       {data.name != undefined &&
       <div className="container">
         <div className="location">
-          
-            <p>{data.name}</p>
-          
-          
-            {data.main ? <h1>{data.main.temp.toFixed()} C</h1> : null}
-          
-          
-          
-            {data.weather ? <p className="bold">{data.weather[0].main}</p> : null}
-      
-            
           <div className="icon">
             {data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img> : null}
-            
-            </div> 
-        </div>
-
-        
-          <div className="bottom">
-          <div className="feels">
-          {data.main ? <p className="bold">{data.main.feels_like.toFixed()}</p> : null}
-            <p>Känns som</p>
           </div>
-          <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}</p> : null}
-            <p>Fuktighet</p>
+          <div className="located">
+            <p>{data.name}</p>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            {data.weather ? <p className="bold">{data.weather[0].main}</p> : null}
           </div>
-          <div className="wind">
-            {data.wind ? <p className="bold">{data.wind.speed} m/s</p> : null}
-            <p>Vind </p>
-          </div>
+          <div className="information">
+            <div className="feel">
+              {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Känns som</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}</p> : null}
+              <p>Fuktighet</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed} m/s</p> : null}
+              <p>Vind </p>
+            </div>
+          </div> 
         </div>
-        </div>
+      </div>
       }
-
     </div>
   );
 }
