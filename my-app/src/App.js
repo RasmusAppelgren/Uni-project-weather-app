ef8439d336b2902382fa0b18bcb9384edb3bd753
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2022-05-23 10:05:16 +0200

    Updated weather.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,55 @@
-import React, {useState} from "react"
-import axios from 'axios'
+import React, {useState, useRef} from "react";
+import Weather from "./weather";
 
 function App() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+    const [weatherLocation, setWeatherLocation] = useState([{
+      id: 1,
+      city: "Stockholm"
+    }]);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=42fdaee782fb69b77ef4995f175ebe38`
+    // Tar input och lägger i variabler
+    const inputLocation = useRef();
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-        console.log(location)
-      })
-      setLocation('')
-    }
-    
-    
+
+    // Först kontrolleras input så det inte är tomt, om tomt skickas prompt
+    function addItem(event){
+      
+      event.preventDefault();
+
+      if (inputLocation.current.value === "") {
+          alert("Du måste ange något!");
+          return false;
+      }
+      
+      const newId = weatherLocation.length > 0 ? weatherLocation[weatherLocation.length - 1].id + 1 : 1;
+      // setWeatherLocation skickar in datan till weatherLocation
+      setWeatherLocation([...weatherLocation, {
+          id: newId,
+          city: inputLocation.current.value     
+      }]);
+      // Rensar fälten efter att filmen är tillagd
+      inputLocation.current.value = "";
+      console.log(weatherLocation)
   }
 
   return (
-    <div className="app">
-      <div className="search">
-        <input value={location} onChange={event => setLocation(event.target.value)} type="text" placeholder="Skriv in plats" onKeyPress={searchLocation}></input>
-      </div>
-      {data.name != undefined &&
-      <div className="container">
-        <div className="location">
-          <div className="icon">
-            {data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img> : null}
-          </div>
-          <div className="located">
-            <p>{data.name}</p>
-            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
-            {data.weather ? <p className="bold">{data.weather[0].main}</p> : null}
-          </div>
-          <div className="information">
-            <div className="feel">
-              {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
-              <p>Känns som</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}</p> : null}
-              <p>Fuktighet</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed} m/s</p> : null}
-              <p>Vind </p>
-            </div>
-          </div> 
+    <div>
+            <form onSubmit={addItem}>
+            <input className="form-control" placeholder="Ange filmtitel här" ref={inputLocation} it="title"></input>
+      
+            
+            <input type="submit" className="btn btn-success mt-3" value="Spara film"></input>
+            </form>
+
+            <ul className="list-group">
+
+                { weatherLocation.map(locate => <Weather key={locate.id} item={locate}/>)}
+                
+     
+            </ul>
+            
         </div>
-      </div>
-      }
-    </div>
-  );
+  )
 }
 
 export default App;
--- my-app/src/weather.js
+++ my-app/src/weather.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&APPID=76d38a6d1f52a05bab65b240fe495e3b'
 
-export default function Weather(props) {
-    function getWeather(grade) {
-        let stars = []
-        for (let i = 0; i < grade; i++ ){
-            stars.push(<img src={star} width="30px" alt="star" className="float-end"></img>)
-        }
-        return stars
-    }
+const Weather = () => {
+    const [data, setData] = useState(null)
 
-    return (
-        <li className="list-group-item">
-            {props.item.title}
-            
-            {getStars(props.item.grade)}
-            
-            <button className="btn btn-sm btn-danger float-end" onClick={() => {props.deleteItem(props.item.id)}}>X</button>
-        </li>
-    )
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    },[])
+
+    if(!data) return null
+
+    console.log(data)
+
+  return (
+    <div className="location">
+          <div className="icon">
+            {data.weather ? <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img> : null}
+          </div>
+          <div className="located">
+            <p>{data.name}</p>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            {data.weather ? <p className="bold">{data.weather[0].main}</p> : null}
+          </div>
+          <div className="information">
+            <div className="feel">
+              {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Känns som</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}</p> : null}
+              <p>Fuktighet</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed} m/s</p> : null}
+              <p>Vind </p>
+            </div>
+          </div> 
+        </div>
+  )
 }
+
+export default Weather
